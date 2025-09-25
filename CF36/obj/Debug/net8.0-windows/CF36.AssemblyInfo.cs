2210d1820f3e9ea0a21477a5fe8c0c7f714adaaa
Hoang Long <nglong1708@gmail.com>
2025-09-25 18:30:31 +0700

    add appconfig

--- CF36/obj/Debug/net8.0-windows/CF36.AssemblyInfo.cs
+++ CF36/obj/Debug/net8.0-windows/CF36.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CF36")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecf56975e41d5940488140a093b861a117893d9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CF36")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CF36")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
